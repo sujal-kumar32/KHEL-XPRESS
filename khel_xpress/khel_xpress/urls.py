9674ed98731edb7156b2a635f7ff44ec650d6b1a
sujal-kumar32 <sujalstark12345@gmail.com>
2025-03-04 10:45:34 +0530

    Updated features and fixed bugs

--- khel_xpress/khel_xpress/urls.py
+++ khel_xpress/khel_xpress/urls.py
@@ -26,5 +26,6 @@ urlpatterns = [
     path('login/', login_view, name='login'),
      path("register/", register_view, name="register"),
       path('logout/', logout_view, name='logout'),
+      
     
 ]
--- khel_xpress/khelxpress/views.py
+++ khel_xpress/khelxpress/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-# Create your views here.
+
+
 def home(request):
 
     return render(request, 'home.html')
@@ -18,7 +19,7 @@ def login_view(request):
         if user is not None:
             login(request, user)
             messages.success(request, "Login successful!")
-            return redirect('/')  # Change to your homepage URL
+            return redirect('/')  
         else:
             messages.error(request, "Invalid username or password.")
             return redirect("login")
@@ -33,43 +34,43 @@ def register_view(request):
         email = request.POST.get("email")
         password = request.POST.get("password")
 
-        # Combine First Name & Last Name to create a Username
+        
         username = f"{first_name}{last_name}".lower()
 
         if not first_name or not last_name or not email or not password:
             messages.error(request, "All fields are required.")
             return redirect("login")
         
-        # Check if user already exists
         if User.objects.filter(username=username).exists():
             messages.error(request, "Username already taken. Try a different one.")
             return redirect("login")
 
 
 
-        # Check if email already exists
-        if User.objects.filter(email=email).exists():
-            messages.error(request, "Email already registered. Try logging in.")
-            return redirect("login")
+
         
-        # Create the user
+   
         user = User.objects.create_user(username=username, email=email, password=password)
         user.first_name = first_name
         user.last_name = last_name
         user.save()
 
-                # Auto-login after registration
-        user = authenticate(request, username=username, password=password)
+  
 
-        # Auto-login after registration
+  
         user = authenticate(request, username=username, password=password)
         if user:
             login(request, user)
-            return redirect("/")  # Redirect to homepage
+            messages.success(request, "Registration successful! You are now logged in.")
+            return redirect("/")  
 
     return render(request, "register.html")
        
 def logout_view(request):
     logout(request)
-    return redirect('/') 
+    messages.success(request, "Logged out successfully.")
+    return redirect('/')
+
+
+
         
